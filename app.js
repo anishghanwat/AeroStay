34bdb25dadc7102f5b5974d80fb4be36b1122001
anishghanwat <anishghanwat.work@gmail.com>
2025-07-19 12:57:19 +0530

    updated README

--- app.js
+++ app.js
@@ -82,7 +82,12 @@ passport.deserializeUser(User.deserializeUser());
 app.use((req, res, next) => {
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
-    res.locals.currentUser = req.user;
+    // Add this middleware before your route handlers
+    app.use((req, res, next) => {
+        res.locals.currentUser = req.user;
+        next();
+    });
+
     next();
 })
 
